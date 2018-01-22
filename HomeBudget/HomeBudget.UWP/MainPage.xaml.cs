f7621e1f7b2f5ee27b8d2568024360b7192c2b22
damian.sikora <dsikora85@gmail.com>
2018-01-22 21:35:36 +0100

    .net standard settings

--- HomeBudget/HomeBudget.UWP/MainPage.xaml.cs
+++ HomeBudget/HomeBudget.UWP/MainPage.xaml.cs
@@ -1,25 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-using System.Threading.Tasks;
-using Windows.UI.Popups;
-using Windows.UI.ViewManagement;
-
-namespace HomeBudget.UWP
+﻿namespace HomeBudget.UWP
 {
-	public sealed partial class MainPage
+    public sealed partial class MainPage
     {
 		public MainPage()
 		{
@@ -28,8 +9,8 @@ namespace HomeBudget.UWP
 
             this.InitializeComponent();
 
-            ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
-            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            //ApplicationView.PreferredLaunchViewSize = new Size(800, 600);
+            //ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 
             LoadApplication(new HomeBudget.App());
 		}
