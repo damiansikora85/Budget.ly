9139189c907df0b98f65e77eefff612cdcfd24af
damian.sikora <dsikora85@gmail.com>
2018-08-09 20:41:45 +0200

    Categories sort comparer to shared proj

--- HomeBudgetShared/Utils/BudgetCategorySortComparer.cs
+++ HomeBudgetShared/Utils/BudgetCategorySortComparer.cs
@@ -2,11 +2,8 @@
 using Syncfusion.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HomeBudget.UWP.Converters
+namespace HomeBudget.Converters
 {
     public class BudgetCategorySortComparer : IComparer<Object>, ISortDirection
     {
