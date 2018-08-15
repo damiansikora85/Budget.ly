513db049253414e601f8b81cada78dc29866f477
damian.sikora <dsikora85@gmail.com>
2018-08-15 19:47:45 +0200

    Underline effect

---
+++ HomeBudget/HomeBudgeStandard/Effects/UnderlineEffect.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace HomeBudgeStandard.Effects
+{
+    public class UnderlineEffect : RoutingEffect
+    {
+        public const string EffectNamespace = "HomeBudgeStandard.Effects";
+
+        public UnderlineEffect() : base($"{EffectNamespace}.{nameof(UnderlineEffect)}")
+        {
+        }
+    }
+}
