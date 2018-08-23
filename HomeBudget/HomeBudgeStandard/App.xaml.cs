080dbaffd79febfcf6d8acb9cd00c9a096ca17ec
damian.sikora <dsikora85@gmail.com>
2018-08-23 21:21:18 +0200

    Back button

--- HomeBudget/HomeBudgeStandard/App.xaml.cs
+++ HomeBudget/HomeBudgeStandard/App.xaml.cs
@@ -11,24 +11,7 @@ namespace HomeBudget
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("OTMwNUAzMTM2MmUzMjJlMzBLM3Q2MWJvZEE2UXR0bFNleUhwMUFESUh2VDB4Wjg0YXljRlM5QjBucDFRPQ==;OTMwNkAzMTM2MmUzMjJlMzBoVHFOWTFERUxDZDBqRkt4Q3NvcDkxdDVmaDg2TndHWU9TWkhVc0R0R1V3PQ==");
             InitializeComponent();
 
-            //SwitchToHomePage();
             MainPage = new MainPage();
-
-            /*if (Device.Idiom == TargetIdiom.Phone)
-            {
-                MainPage = new HomeBudget.MainPage();
-            }
-            if (Device.Idiom == TargetIdiom.Tablet)
-            {
-                MainPage = new HomeBudget.MainPage();
-            }
-            else
-            {
-                MainPage = new HomeBudget.MainPagePC();// new NavigationPage(new HomeBudget.MainPagePC());
-                //MainPage = new HomeBudget.Pages.PC.MainPagePCNew();
-            }*/
-
-
         }
 
 		protected override void OnStart()
@@ -45,5 +28,11 @@ namespace HomeBudget
 		{
 			// Handle when your app resumes
 		}
+
+        public bool OnBackPressed()
+        {
+            var masterDetail = MainPage as MainPage;
+            return masterDetail.OnBackPressed();
+        }
 	}
 }
