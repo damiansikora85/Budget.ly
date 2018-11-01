f32d47513559d3d2031db4c9aaf949d9340c9a44
damian.sikora <dsikora85@gmail.com>
2018-11-01 21:20:03 +0100

    Update UWP

--- HomeBudget/HomeBudget.UWP/Pages/SettingsPage.xaml.cs
+++ HomeBudget/HomeBudget.UWP/Pages/SettingsPage.xaml.cs
@@ -1,12 +1,8 @@
-﻿using Dropbox.Api;
-using Dropbox.Api.Files;
-using HomeBudget.Code;
+﻿using HomeBudget.UWP.Utils;
 using System;
-using System.Threading.Tasks;
-using Windows.Security.Authentication.Web;
-using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -18,111 +14,119 @@ namespace HomeBudget.UWP.Pages
     /// 
     public sealed partial class SettingsPage : Page
     {
-        private const string RedirectUri = "https://localhost/authorize/";
-        private string appKey = "p6cayskxetnkx1a";
-        private string oauth2State;
-        private bool _checkDropboxFileExist;
-
         public SettingsPage()
         {
             this.InitializeComponent();
-            ProgressRing.IsActive = false;
         }
 
-        private async void ConnectWithDropbox(object sender, RoutedEventArgs e)
+        private void NotificationCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            _checkDropboxFileExist = false;
-            await LoginToDropbox();
+            NotificationsSetEnabled(false);
         }
 
-        private async void ConnectWithDataCheck(object sender, RoutedEventArgs e)
+        private void notificationCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            _checkDropboxFileExist = true;
-            await LoginToDropbox();
+            NotificationsSetEnabled(true);
         }
 
-        private async Task LoginToDropbox()
+        private void NotificationsSetEnabled(bool enabled)
         {
-            oauth2State = Guid.NewGuid().ToString("N");
-            var authorizeUri = DropboxOAuth2Helper.GetAuthorizeUri(OAuthResponseType.Token, appKey, new Uri(RedirectUri), state: oauth2State);
-
-            var result = await WebAuthenticationBroker.AuthenticateAsync(
-                WebAuthenticationOptions.None,
-                authorizeUri,
-                new Uri(RedirectUri));
-
-            await ProcessResult(result);
+            checkBoxMon.IsEnabled = enabled;
+            checkBoxTue.IsEnabled = enabled;
+            checkBoxWed.IsEnabled = enabled;
+            checkBoxThu.IsEnabled = enabled;
+            checkBoxFri.IsEnabled = enabled;
+            checkBoxSat.IsEnabled = enabled;
+            checkBoxSun.IsEnabled = enabled;
+            timePicker.IsEnabled = enabled;
         }
 
-        private async Task ProcessResult(WebAuthenticationResult result)
+        private void SaveClick(object sender, RoutedEventArgs e)
         {
-            switch (result.ResponseStatus)
+            SaveNotificationSettings();
+
+            var notificationManager = new NotificationManager();
+            notificationManager.ReScheduleNotificationsBySettings();
+        }
+
+        private void SaveNotificationSettings()
+        {
+            SaveSetting("NotificationsDisabled", notificationCheckBox.IsChecked.HasValue ? notificationCheckBox.IsChecked.Value : false);
+            SaveSetting("NotificationsTime", timePicker.Time);
+
+            SaveSetting("NotificationsMonday", checkBoxMon.IsChecked.HasValue ? checkBoxMon.IsChecked.Value : false);
+            SaveSetting("NotificationsTuesday", checkBoxTue.IsChecked.HasValue ? checkBoxTue.IsChecked.Value : false);
+            SaveSetting("NotificationsWednesday", checkBoxWed.IsChecked.HasValue ? checkBoxWed.IsChecked.Value : false);
+            SaveSetting("NotificationsThursday", checkBoxThu.IsChecked.HasValue ? checkBoxThu.IsChecked.Value : false);
+            SaveSetting("NotificationsFriday", checkBoxFri.IsChecked.HasValue ? checkBoxFri.IsChecked.Value : false);
+            SaveSetting("NotificationsSaturday", checkBoxSat.IsChecked.HasValue ? checkBoxSat.IsChecked.Value : false);
+            SaveSetting("NotificationsSunday", checkBoxSun.IsChecked.HasValue ? checkBoxSun.IsChecked.Value : false);
+        }
+
+        private void RestoreFromSettings()
+        {
+            var notificationsDisabled = GetFromSettings("NotificationsDisabled");
+            if (notificationsDisabled is bool check)
+                notificationCheckBox.IsChecked = check;
+
+            var notificationTime = GetFromSettings("NotificationsTime");
+            if (notificationTime is TimeSpan notificationTimeValue)
+                timePicker.Time = notificationTimeValue;
+
+            var notificationMonday = GetFromSettings("NotificationsMonday");
+            if (notificationMonday is bool notificationMondayValue)
+                checkBoxMon.IsChecked = notificationMondayValue;
+
+            var notificationTuesday = GetFromSettings("NotificationsTuesday");
+            if (notificationTuesday is bool notificationTuesdayValue)
+                checkBoxTue.IsChecked = notificationTuesdayValue;
+
+            var notificationWednesday = GetFromSettings("NotificationsWednesday");
+            if (notificationWednesday is bool notificationWednesdayValue)
+                checkBoxWed.IsChecked = notificationWednesdayValue;
+
+            var notificationThursday = GetFromSettings("NotificationsThursday");
+            if (notificationThursday is bool notificationThursdayValue)
+                checkBoxThu.IsChecked = notificationThursdayValue;
+
+            var notificationFriday = GetFromSettings("NotificationsFriday");
+            if (notificationFriday is bool notificationFridayValue)
+                checkBoxFri.IsChecked = notificationFridayValue;
+
+            var notificationSaturday = GetFromSettings("NotificationsSaturday");
+            if (notificationSaturday is bool notificationSaturdayValue)
+                checkBoxSat.IsChecked = notificationSaturdayValue;
+
+            var notificationSunday = GetFromSettings("NotificationsSunday");
+            if (notificationSunday is bool notificationSundayValue)
+                checkBoxSun.IsChecked = notificationSundayValue;
+        }
+
+        private void SaveSetting(string settingName, object value)
+        {
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+
+            if (localSettings.Values.ContainsKey(settingName))
+                localSettings.Values[settingName] = value;
+            else
+                localSettings.Values.Add(settingName, value);
+        }
+
+        private object GetFromSettings(string settingName)
+        {
+            Windows.Storage.ApplicationDataContainer localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            if (localSettings.Values.ContainsKey(settingName))
             {
-                case WebAuthenticationStatus.Success:
-                    {
-                        ProgressRing.IsActive = true;
-                        var response = DropboxOAuth2Helper.ParseTokenFragment(new Uri(result.ResponseData));
-                        Helpers.Settings.DropboxAccessToken = response.AccessToken;
-                        if (!_checkDropboxFileExist)
-                            MainBudget.Instance.OnCloudStorageConnected();
-                        else if (await HasDropboxData())
-                            MainBudget.Instance.OnCloudStorageConnected();
-                        else
-                        {
-                            ProgressRing.IsActive = false;
-                            ShowToastNotification("HomeBudget", "Dropbox data not found");
-                        }
-
-                    }
-                    break;
-
-                /*case WebAuthenticationStatus.ErrorHttp:
-                    throw new OAuthException(result.ResponseErrorDetail);
-
-                case WebAuthenticationStatus.UserCancel:
-                default:
-                    throw new OAuthUserCancelledException();*/
+                return localSettings.Values[settingName];
             }
+            else
+                return null;
         }
 
-        private async Task<bool> HasDropboxData()
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            var accessToken = HomeBudget.Helpers.Settings.DropboxAccessToken;
-            var hasData = false;
-            using (var dropboxClient = new DropboxClient(accessToken))
-            {
-                try
-                {
-                    var metadata = await dropboxClient.Files.GetMetadataAsync("fake.dat");// DropboxCloudStorage.DROPBOX_DATA_FILE_PATH);
-                    hasData = metadata.IsFile;
-                }
-                catch (ApiException<GetMetadataError> apiExc)
-                {
-                    hasData = false;
-                }
-                catch (Exception e)
-                {
-                    hasData = false;
-                }
-            }
-
-            return hasData;
-        }
-
-        private void ShowToastNotification(string title, string stringContent)
-        {
-            ToastNotifier ToastNotifier = ToastNotificationManager.CreateToastNotifier();
-            Windows.Data.Xml.Dom.XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastText02);
-            Windows.Data.Xml.Dom.XmlNodeList toastNodeList = toastXml.GetElementsByTagName("text");
-            toastNodeList.Item(0).AppendChild(toastXml.CreateTextNode(title));
-            toastNodeList.Item(1).AppendChild(toastXml.CreateTextNode(stringContent));
-            Windows.Data.Xml.Dom.IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
-            Windows.Data.Xml.Dom.XmlElement audio = toastXml.CreateElement("audio");
-            audio.SetAttribute("src", "ms-winsoundevent:Notification.SMS");
-
-            ToastNotification toast = new ToastNotification(toastXml);
-            toast.ExpirationTime = DateTime.Now.AddSeconds(4);
-            ToastNotifier.Show(toast);
+            RestoreFromSettings();
+            base.OnNavigatedTo(e);
         }
     }
 }
---
+++ HomeBudget/HomeBudget.UWP/Utils/NotificationManager.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace HomeBudget.UWP.Utils
+{
+    public class NotificationManager
+    {
+        private ToastNotifier _notificationManager;
+        private Windows.Storage.ApplicationDataContainer _localSettings;
+
+        public NotificationManager()
+        {
+            _notificationManager = ToastNotificationManager.CreateToastNotifier();
+            _localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+        }
+
+        public void ReScheduleNotificationsBySettings()
+        {
+            ClearAllNotifications();
+            if (GetFromSettings("NotificationsDisabled") is bool notificationsDisabled && !notificationsDisabled)
+            {
+                if (GetFromSettings("NotificationsTime") is TimeSpan notificationTime)
+                {
+                    if (GetFromSettings("NotificationsMonday") is bool notificationsMonday && notificationsMonday)
+                        ScheduleNotification(DayOfWeek.Monday, notificationTime);
+
+                    if (GetFromSettings("NotificationsTuesday") is bool notificationsTuesday && notificationsTuesday)
+                        ScheduleNotification(DayOfWeek.Tuesday, notificationTime);
+
+                    if (GetFromSettings("NotificationsWednesday") is bool notificationsWednesday && notificationsWednesday)
+                        ScheduleNotification(DayOfWeek.Wednesday, notificationTime);
+
+                    if (GetFromSettings("NotificationsThursday") is bool notificationsThursday && notificationsThursday)
+                        ScheduleNotification(DayOfWeek.Thursday, notificationTime);
+
+                    if (GetFromSettings("NotificationsFriday") is bool notificationsFriday && notificationsFriday)
+                        ScheduleNotification(DayOfWeek.Friday, notificationTime);
+
+                    if (GetFromSettings("NotificationsSaturday") is bool notificationsSaturday && notificationsSaturday)
+                        ScheduleNotification(DayOfWeek.Saturday, notificationTime);
+
+                    if (GetFromSettings("NotificationsSunday") is bool notificationsSunday && notificationsSunday)
+                        ScheduleNotification(DayOfWeek.Sunday, notificationTime);
+                }
+            }
+        }
+
+        private object GetFromSettings(string settingName)
+        {
+            if (_localSettings.Values.ContainsKey(settingName))
+            {
+                return _localSettings.Values[settingName];
+            }
+            else
+                return null;
+        }
+
+        public void ClearAllNotifications()
+        {
+            if (_notificationManager != null)
+            {
+                var scheduledNotifications = _notificationManager.GetScheduledToastNotifications();
+                foreach (var notification in scheduledNotifications)
+                    _notificationManager.RemoveFromSchedule(notification);
+            }
+        }
+
+        public void ScheduleNotification(DayOfWeek dayOfWeek, TimeSpan time)
+        {
+            var now = DateTime.Now;
+            ScheduledToastNotification notification = null;
+            var notificationDate = GetNextWeekday(now, dayOfWeek).Date + time;
+            if (notificationDate > now)
+            {
+                notification = CreateNotification(notificationDate);
+                _notificationManager.AddToSchedule(notification);
+            }
+            notification = CreateNotification(notificationDate.AddDays(7));
+            _notificationManager.AddToSchedule(notification);
+
+            notification = CreateNotification(notificationDate.AddDays(14));
+            _notificationManager.AddToSchedule(notification);
+
+            notification = CreateNotification(notificationDate.AddDays(21));
+            _notificationManager.AddToSchedule(notification);
+
+            notification = CreateNotification(notificationDate.AddDays(28));
+            _notificationManager.AddToSchedule(notification);
+            
+        }
+
+        public static DateTime GetNextWeekday(DateTime start, DayOfWeek day)
+        {
+            // The (... + 7) % 7 ensures we end up with a value in the range [0, 6]
+            int daysToAdd = ((int)day - (int)start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
+        }
+
+        private ScheduledToastNotification CreateNotification(DateTime dateTime)
+        {
+            var visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = "Budget.ly"
+                        },
+
+                        new AdaptiveText()
+                        {
+                            Text = "Notification test"
+                        },
+                    },
+                }
+            };
+
+            // Now we can construct the final toast content
+            ToastContent toastContent = new ToastContent()
+            {
+                Visual = visual,
+            };
+
+            // And create the toast notification
+            return new ScheduledToastNotification(toastContent.GetXml(), new DateTimeOffset(dateTime));
+        }
+    }
+}
