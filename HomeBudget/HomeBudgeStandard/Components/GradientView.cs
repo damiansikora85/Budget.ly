cf01bbc599196c798593959e1bb7f084c9487462
Damian Sikora <dsikora85@gmail.com>
2019-01-23 21:19:42 +0100

    GradientView component

---
+++ HomeBudget/HomeBudgeStandard/Components/GradientView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HomeBudget.Components
+{
+    public class GradientView : View
+    {
+        public static readonly BindableProperty StartColorProperty = BindableProperty.Create(nameof(StartColor), typeof(Color), typeof(GradientView), Color.Red);
+        public static readonly BindableProperty EndColorProperty = BindableProperty.Create(nameof(EndColor), typeof(Color), typeof(GradientView), Color.White);
+
+        public Color StartColor
+        {
+            get => (Color)GetValue(StartColorProperty);
+            set => SetValue(StartColorProperty, value);
+        }
+
+        public Color EndColor
+        {
+            get => (Color)GetValue(EndColorProperty);
+            set => SetValue(EndColorProperty, value);
+        }
+    }
+}
