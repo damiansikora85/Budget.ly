448dab99e9fcb9c4236c01a0011e9e453a6821ed
Damian Sikora <dsikora85@gmail.com>
2019-03-27 20:48:28 +0100

    Set 100% if planned value not set

--- HomeBudgetShared/Utils/BudgetSummaryListView.cs
+++ HomeBudgetShared/Utils/BudgetSummaryListView.cs
@@ -101,7 +101,7 @@ namespace HomeBudget.Pages.Utils
         public string IconFile { get; set; }
         public double SpendPercentage =>
                 //Random rand = new Random();
-                CategoryPlanned.TotalValues > 0 ? Math.Min((CategoryReal.TotalValues / CategoryPlanned.TotalValues), 1) : 0; 
+                CategoryPlanned.TotalValues > 0 ? Math.Min((CategoryReal.TotalValues / CategoryPlanned.TotalValues), 1) : 1; 
 
         public int SpendPercentageInt
         {
