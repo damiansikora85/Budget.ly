c9cd95856f98539e9ebb22e816fb3f7516b240d6
Damian Sikora <dsikora85@gmail.com>
2019-04-02 20:59:44 +0200

    Custom version

--- HomeBudget/HomeBudget.Droid/MainActivity.cs
+++ HomeBudget/HomeBudget.Droid/MainActivity.cs
@@ -14,7 +14,7 @@ using Xamarin.Forms;
 
 namespace HomeBudget.Droid
 {
-    [Activity(Label = "Budget.ly", Icon = "@drawable/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    [Activity(Label = "Budget.ly[x]", Icon = "@drawable/icon", Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
         private App _theApp;
