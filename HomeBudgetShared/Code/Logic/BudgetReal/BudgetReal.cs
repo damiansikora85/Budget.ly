7f4dd2ed5be785e598e44702050335542e818462
Damian Sikora <dsikora85@gmail.com>
2019-08-13 21:01:36 +0200

    Transactions WIP

--- HomeBudgetShared/Code/Logic/BudgetReal/BudgetReal.cs
+++ HomeBudgetShared/Code/Logic/BudgetReal/BudgetReal.cs
@@ -12,6 +12,8 @@ namespace HomeBudget.Code.Logic
     [ProtoContract]
     public sealed class BudgetReal : BaseBudget.BaseBudget
     {
+        public List<BudgetTransaction> Transactions { get; set; }
+
         public void Setup(List<BudgetCategoryTemplate> categoriesDesc)
         {
             foreach (BudgetCategoryTemplate categoryDesc in categoriesDesc)
@@ -26,12 +28,18 @@ namespace HomeBudget.Code.Logic
         {
             var category = (BudgetRealCategory)GetBudgetCategory(categoryId);
             category.AddValue(value, date, subcatId);
+            
         }
 
         public void AddIncome(double value, DateTime date, int incomeCategoryId)
         {
             var category = (BudgetRealCategory)GetIncomesCategories()[0];
             category.AddValue(value, date, incomeCategoryId);
+        }
+
+        private void AddTransaction(double value, DateTime date, int categoryId, int subcatId, string note)
+        {
+            Transactions.Add(new BudgetTransaction { Amount = value, CategoryId = categoryId, SubcatId = subcatId, Date = date, Note = note });
         }
     }
 }
