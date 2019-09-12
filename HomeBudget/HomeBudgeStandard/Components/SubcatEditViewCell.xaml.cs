801021f328ca673f0995c0d40f5b1a5f6a1ea17f
Damian Sikora <dsikora85@gmail.com>
2019-09-12 20:43:45 +0200

    Missing files

---
+++ HomeBudget/HomeBudgeStandard/Components/SubcatEditViewCell.xaml.cs
@@ -0,0 +1,40 @@
+﻿using HomeBudget.Code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeBudgeStandard.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SubcatEditViewCell : ViewCell
+    {
+        public event EventHandler<BudgetSubcatEdit> OnRemove;
+
+        public SubcatEditViewCell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            if(BindingContext is BudgetSubcatEdit data && data.IsNew)
+            {
+                Device.BeginInvokeOnMainThread(() => entry.Focus());
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (BindingContext is BudgetSubcatEdit data)
+            {
+                OnRemove?.Invoke(this, data);
+            }
+        }
+    }
+}
