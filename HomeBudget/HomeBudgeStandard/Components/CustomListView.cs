2d42a22ae052153fb7c3e22aa5b50aa42d1997e2
Damian Sikora <dsikora85@gmail.com>
2020-01-06 16:14:59 +0100

    Fixed dinamic size summary header

---
+++ HomeBudget/HomeBudgeStandard/Components/CustomListView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HomeBudgeStandard.Components
+{
+    public class CustomListView : ListView
+    {
+        public static BindableProperty FirstElementVisibiltyPercProperty = BindableProperty.Create(nameof(FirstElementVisibiltyPerc), typeof(float), typeof(CustomListView));
+        public static BindableProperty ScrollPositionProperty = BindableProperty.Create(nameof(ScrollPosition), typeof(int), typeof(CustomListView));
+        public float FirstElementVisibiltyPerc
+        {
+            get => (float)GetValue(FirstElementVisibiltyPercProperty);
+            set => SetValue(FirstElementVisibiltyPercProperty, value);
+        }
+
+        public int ScrollPosition
+        {
+            get => (int)GetValue(ScrollPositionProperty);
+            set => SetValue(ScrollPositionProperty, value);
+        }
+
+        public int Test { get; set; }
+
+        public event EventHandler OnScroll;
+
+        public void WasScrolled()
+        {
+            OnScroll?.Invoke(this, null);
+        }
+    }
+}
