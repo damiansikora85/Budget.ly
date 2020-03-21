6119970b55a7d7c16fd5c6575737fc25d4e8fbc6
Damian Sikora <dsikora85@gmail.com>
2020-03-21 12:59:26 +0100

    Modified summary tabs

---
+++ HomeBudget/HomeBudgeStandard/Views/SummaryTabsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HomeBudgeStandard.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SummaryTabsView : ContentView
+    {
+        public SummaryTabsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
