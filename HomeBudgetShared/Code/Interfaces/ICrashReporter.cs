b28d6620b610ec9344bd8236afbbf6143789e711
Damian Sikora <dsikora85@gmail.com>
2020-05-14 20:27:43 +0200

    Added crash reporter

---
+++ HomeBudgetShared/Code/Interfaces/ICrashReporter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeBudgetShared.Code.Interfaces
+{
+    public interface ICrashReporter
+    {
+        void Report(Exception exc);
+    }
+}
