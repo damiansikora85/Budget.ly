2c662a0acf0ab662482901982de420a2a47adfee
Damian Sikora <dsikora85@gmail.com>
2020-06-05 20:37:24 +0200

    Tests

--- HomeBudget.UnitTestsF/Tests.fs
+++ HomeBudget.UnitTestsF/Tests.fs
@@ -8,18 +8,19 @@ open Moq
 open HomeBudget.Code.Logic
 open HomeBudgetShared.Code.Synchronize
 open HomeBudgetShared.Code.Interfaces
+open HomeBudget.Code.Interfaces
 
 [<TestClass>]
 type TestClass () =
 
     [<TestMethod>]
     member this.TestBudgetInit () =
-        MainBudget.Instance.Init(Mock<IFileManager>().Object, Mock<IBudgetSynchronizer>().Object, Mock<ICrashReporter>().Object)
+        MainBudget.Instance.Init(Mock<IFileManager>().Object, Mock<IBudgetSynchronizer>().Object, Mock<ICrashReporter>().Object, Mock<ISettings>().Object)
         Assert.IsNotNull(MainBudget.Instance);
 
     [<TestMethod>]
     member this.AddExpenseTest () =
-        MainBudget.Instance.Init(Mock<IFileManager>().Object, Mock<IBudgetSynchronizer>().Object, Mock<ICrashReporter>().Object)
+        MainBudget.Instance.Init(Mock<IFileManager>().Object, Mock<IBudgetSynchronizer>().Object, Mock<ICrashReporter>().Object, Mock<ISettings>().Object)
 
         let currentMonth = MainBudget.Instance.GetCurrentMonthData()
         let category = currentMonth.BudgetReal.Categories.[0]
